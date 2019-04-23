baab42f48ac00c119e93f042f3722cf315c2b978
David Riddell <davidriddell13@gmail.com>
2019-04-24 08:22:06 +1000

    local host udp connection

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 import socket
 
 class host():
-    def __init__():
+    def __init__(self):
+        self.host_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.host_socket.bind(('localhost', 0))
+        print("Server listening")
+        print(self.host_socket.getsockname()[1])
+        print(self.host_socket.getsockname()[0])
 
-        
-        
+        while(True):
+            conn, addr = self.host_socket.recvfrom(4096)
+            print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
+        self.host_socket.close()
+  
 def main():
-    pass
+    server = host()
 
-if if __name__ == "__main__":
-    pass
+if __name__ == "__main__":
+    main()
